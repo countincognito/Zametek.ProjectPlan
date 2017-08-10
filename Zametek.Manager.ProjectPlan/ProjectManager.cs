770bdf7d332af9f742af960497e3602c992949c3
Riccardo Bennett-Lovsey <countincognito@outlook.com>
2017-08-10 16:58:42 +0100

    Zametek.Manager.ProjectPlan ported.

---
+++ Zametek.Manager.ProjectPlan/ProjectManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Zametek.Common.Project;
+using Zametek.Common.ProjectPlan;
+using Zametek.Contract.ProjectPlan;
+using Zametek.Maths.Graphs;
+
+namespace Zametek.Manager.ProjectPlan
+{
+    public class ProjectManager
+        : IProjectManager
+    {
+        #region Fields
+
+        private readonly IGraphProcessingEngine m_GraphProcessingEngine;
+        private readonly IMetricAssessingEngine m_MetricAssessingEngine;
+
+        #endregion
+
+        #region Ctors
+
+        public ProjectManager(
+            IGraphProcessingEngine graphProcessingEngine,
+            IMetricAssessingEngine metricAssessingEngine)
+        {
+            m_GraphProcessingEngine = graphProcessingEngine ?? throw new ArgumentNullException(nameof(graphProcessingEngine));
+            m_MetricAssessingEngine = metricAssessingEngine ?? throw new ArgumentNullException(nameof(metricAssessingEngine));
+        }
+
+        #endregion
+
+        #region IProjectManager Members
+
+        public MetricsDto CalculateProjectMetrics(IList<IActivity<int>> activities, IList<ActivitySeverityDto> activitySeverityDtos)
+        {
+            if (activities == null)
+            {
+                throw new ArgumentNullException(nameof(activities));
+            }
+            if (activitySeverityDtos == null)
+            {
+                throw new ArgumentNullException(nameof(activitySeverityDtos));
+            }
+            return m_MetricAssessingEngine.CalculateProjectMetrics(activities, activitySeverityDtos);
+        }
+
+        public byte[] ExportArrowGraphToDiagram(DiagramArrowGraphDto diagramArrowGraphDto)
+        {
+            if (diagramArrowGraphDto == null)
+            {
+                throw new ArgumentNullException(nameof(diagramArrowGraphDto));
+            }
+            return m_GraphProcessingEngine.ExportArrowGraphToDiagram(diagramArrowGraphDto);
+        }
+
+        #endregion
+    }
+}
---
+++ Zametek.Manager.ProjectPlan/SettingManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Zametek.Common.Project;
+using Zametek.Contract.ProjectPlan;
+
+namespace Zametek.Manager.ProjectPlan
+{
+    public class SettingManager
+        : ISettingManager
+    {
+        #region Fields
+        
+        private readonly ISettingResourceAccess m_SettingResourceAccess;
+
+        #endregion
+
+        #region Ctors
+
+        public SettingManager(ISettingResourceAccess settingResourceAccess)
+        {
+            m_SettingResourceAccess = settingResourceAccess ?? throw new ArgumentNullException(nameof(settingResourceAccess));
+        }
+
+        #endregion
+
+        #region ISettingManager Members
+
+        public ArrowGraphSettingsDto GetArrowGraphSettings()
+        {
+            return m_SettingResourceAccess.GetArrowGraphSettings();
+        }
+
+        #endregion
+    }
+}
