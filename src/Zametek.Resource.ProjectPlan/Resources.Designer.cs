564dcafb3fba28b9176b9e473cde3e56a4b01473
countincognito <countincognito@outlook.com>
2020-09-06 23:26:07 +0100

    Update to v0.6.1

--- src/Zametek.Resource.ProjectPlan/Resources.Designer.cs
+++ src/Zametek.Resource.ProjectPlan/Resources.Designer.cs
@@ -160,7 +160,7 @@ namespace Zametek.Resource.ProjectPlan {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Version 0.6.0.
+        ///   Looks up a localized string similar to Version 0.6.1.
         /// </summary>
         public static string Label_AppVersion {
             get {
