c7f3563a9c3ead8a1c223d6b6248cb56f0073b78
countincognito <countincognito@outlook.com>
2023-08-14 21:58:03 +0100

    Convert Mvvm to use Reactive again.

--- src/Zametek.ProjectPlan/DockFactory.cs
+++ src/Zametek.ProjectPlan/DockFactory.cs
@@ -1,8 +1,8 @@
 ﻿using Dock.Avalonia.Controls;
 using Dock.Model.Controls;
 using Dock.Model.Core;
-using Dock.Model.Mvvm;
-using Dock.Model.Mvvm.Controls;
+using Dock.Model.ReactiveUI;
+using Dock.Model.ReactiveUI.Controls;
 using System;
 using System.Collections.Generic;
 using Zametek.Contract.ProjectPlan;
--- src/Zametek.ViewModel.ProjectPlan/ToolViewModelBase.cs
+++ src/Zametek.ViewModel.ProjectPlan/ToolViewModelBase.cs
@@ -1,62 +1,9 @@
-using Dock.Model.Mvvm.Controls;
-using ReactiveUI;
-using System.ComponentModel;
-using System.Reactive;
+using Dock.Model.ReactiveUI.Controls;
 
 namespace Zametek.ViewModel.ProjectPlan
 {
     public class ToolViewModelBase
-        : Tool, IReactiveObject
+        : Tool
     {
-        private readonly Lazy<Unit> _propertyChangingEventsSubscribed;
-        private readonly Lazy<Unit> _propertyChangedEventsSubscribed;
-
-        public ToolViewModelBase()
-        {
-            _propertyChangingEventsSubscribed = new Lazy<Unit>(
-                                                               () =>
-                                                               {
-                                                                   this.SubscribePropertyChangingEvents();
-                                                                   return Unit.Default;
-                                                               },
-                                                               LazyThreadSafetyMode.PublicationOnly);
-            _propertyChangedEventsSubscribed = new Lazy<Unit>(
-                                                              () =>
-                                                              {
-                                                                  this.SubscribePropertyChangedEvents();
-                                                                  return Unit.Default;
-                                                              },
-                                                              LazyThreadSafetyMode.PublicationOnly);
-        }
-
-        public event PropertyChangingEventHandler? PropertyChanging
-        {
-            add
-            {
-                _ = _propertyChangingEventsSubscribed.Value;
-                PropertyChangingHandler += value;
-            }
-            remove => PropertyChangingHandler -= value;
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged
-        {
-            add
-            {
-                _ = _propertyChangedEventsSubscribed.Value;
-                PropertyChangedHandler += value;
-            }
-            remove => PropertyChangedHandler -= value;
-        }
-
-        private event PropertyChangingEventHandler? PropertyChangingHandler;
-
-        private event PropertyChangedEventHandler? PropertyChangedHandler;
-
-        void IReactiveObject.RaisePropertyChanging(PropertyChangingEventArgs args) =>
-            PropertyChangingHandler?.Invoke(this, args);
-
-        void IReactiveObject.RaisePropertyChanged(PropertyChangedEventArgs args) =>
-            PropertyChangedHandler?.Invoke(this, args);
     }
 }
