261a50c0755fa5b7d9b318719bc0a2caa3f9f02d
countincognito <countincognito@outlook.com>
2024-07-09 23:56:54 +0100

    First checkin for CLI.

---
+++ src/Zametek.ProjectPlan.CommandLine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Zametek.ProjectPlan.CommandLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
