92d534bd1be691ec733c4fa6e421680879af56ba
countincognito <countincognito@outlook.com>
2024-11-27 00:41:52 +0000

    Update libraries. First attempt to zoom on arrow graph with mouse pointer at the centre of the zoom.

--- src/Zametek.View.ProjectPlan/ArrowGraphManagement/ArrowGraphManagerView.axaml.cs
+++ src/Zametek.View.ProjectPlan/ArrowGraphManagement/ArrowGraphManagerView.axaml.cs
@@ -10,7 +10,8 @@ namespace Zametek.View.ProjectPlan
     public partial class ArrowGraphManagerView
         : UserControl
     {
-        private Point? m_LastDragPoint;
+        private Point? m_LastDragPoint = new Point();
+        private Point m_CurrentPoint = new();
         private const double c_SliderDelta = 0.1;
 
         public ArrowGraphManagerView()
@@ -22,16 +23,20 @@ namespace Zametek.View.ProjectPlan
         {
             ArgumentNullException.ThrowIfNull(e);
             var scrollViewer = sender as ScrollViewer;
-            if (scrollViewer is not null
-                && e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            if (scrollViewer is not null)
             {
-                if (m_LastDragPoint.HasValue)
+                Point posNow = e.GetPosition(scrollViewer);
+                m_CurrentPoint = posNow;
+
+                if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
                 {
-                    Point posNow = e.GetPosition(scrollViewer);
-                    double dX = posNow.X - m_LastDragPoint.Value.X;
-                    double dY = posNow.Y - m_LastDragPoint.Value.Y;
-                    m_LastDragPoint = posNow;
-                    scrollViewer.Offset = new Vector(scrollViewer.Offset.X - dX, scrollViewer.Offset.Y - dY);
+                    if (m_LastDragPoint.HasValue)
+                    {
+                        double dX = posNow.X - m_LastDragPoint.Value.X;
+                        double dY = posNow.Y - m_LastDragPoint.Value.Y;
+                        m_LastDragPoint = posNow;
+                        scrollViewer.Offset = new Vector(scrollViewer.Offset.X - dX, scrollViewer.Offset.Y - dY);
+                    }
                 }
             }
         }
@@ -55,12 +60,12 @@ namespace Zametek.View.ProjectPlan
             if (scrollViewer is not null
                 && e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
             {
-                var mousePos = e.GetPosition(scrollViewer);
-                if (mousePos.X <= scrollViewer.Viewport.Width
-                    && mousePos.Y < scrollViewer.Viewport.Height) //make sure we still can use the scrollbars
+                Point pointer = e.GetPosition(scrollViewer);
+                if (pointer.X <= scrollViewer.Viewport.Width
+                    && pointer.Y < scrollViewer.Viewport.Height) //make sure we still can use the scrollbars
                 {
                     scrollViewer.Cursor = new Cursor(StandardCursorType.SizeAll);
-                    m_LastDragPoint = mousePos;
+                    m_LastDragPoint = pointer;
                 }
             }
         }
@@ -68,6 +73,10 @@ namespace Zametek.View.ProjectPlan
         private void Zoom_PointerWheelChanged(object? sender, PointerWheelEventArgs e)
         {
             ArgumentNullException.ThrowIfNull(e);
+
+            double oldZoom = zoomer.Value;
+            Vector oldOffset = viewer.Offset;
+
             if (e.Delta.Y > 0)
             {
                 zoomer.Value += c_SliderDelta;
@@ -76,6 +85,23 @@ namespace Zametek.View.ProjectPlan
             {
                 zoomer.Value -= c_SliderDelta;
             }
+
+            double newZoom = zoomer.Value;
+
+            // Reposition the scrollviewer to center the image zoom
+            // where the mouse pointer is.
+
+            double factor = newZoom / oldZoom;
+
+            double xCorrection = (m_CurrentPoint.X * (newZoom - oldZoom)) / oldZoom;
+
+            double yCorrection = (m_CurrentPoint.Y * (newZoom - oldZoom)) / oldZoom;
+
+            var newOffset = new Vector(
+                (oldOffset.X * factor) + xCorrection,
+                (oldOffset.Y * factor) + yCorrection);
+
+            viewer.Offset = newOffset;
             e.Handled = true;
         }
     }
