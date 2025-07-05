489d3fad09939c4d34a4845a7e7fd3bd97057db2
countincognito <countincognito@outlook.com>
2025-07-05 17:53:19 +0100

    Update version to v0.9.2

--- src/Zametek.Resource.ProjectPlan/Labels.Designer.cs
+++ src/Zametek.Resource.ProjectPlan/Labels.Designer.cs
@@ -196,7 +196,7 @@ namespace Zametek.Resource.ProjectPlan {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.9.1.
+        ///   Looks up a localized string similar to 0.9.2.
         /// </summary>
         public static string Label_AppVersion {
             get {
