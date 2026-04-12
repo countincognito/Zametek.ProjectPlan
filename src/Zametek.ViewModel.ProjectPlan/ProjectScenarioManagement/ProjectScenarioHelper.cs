76215f31d503255ebfad78b31686357f43008f56
countincognito <countincognito@outlook.com>
2026-04-12 12:45:57 +0100

    Add more tests for edge cases in ID reordering.

--- src/Zametek.ViewModel.ProjectPlan/ProjectScenarioManagement/ProjectScenarioHelper.cs
+++ src/Zametek.ViewModel.ProjectPlan/ProjectScenarioManagement/ProjectScenarioHelper.cs
@@ -58,7 +58,7 @@ namespace Zametek.ViewModel.ProjectPlan
                 Debug.Assert(originalIds.Contains(fromId));
             }
 
-            List<int> mapIds = [.. originalIds.Union(idUpdates.Select(x => x.ToId)).Distinct().Order()];
+            List<int> mapIds = [.. originalIds.Union(idUpdates.Select(x => x.FromId)).Distinct().Order()];
             List<IdMap> maps = [];
 
             foreach (int id in mapIds)
@@ -74,28 +74,12 @@ namespace Zametek.ViewModel.ProjectPlan
                 maps.Add(new IdMap { FromId = fromId, ToId = toId, Locked = locked });
             }
 
-            ApplyUpdate(maps);
-
-
-
-
+            ApplyMaps(maps);
 
             return [.. maps.OrderBy(m => m.FromId).Select(x => (x.FromId, x.ToId))];
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-        private static void ApplyUpdate(List<IdMap> maps)
+        private static void ApplyMaps(List<IdMap> maps)
         {
             // Make sure the mappings are ordered by the old IDs.
             maps.Sort(s_IdMapComparer);
@@ -116,35 +100,64 @@ namespace Zametek.ViewModel.ProjectPlan
                     continue;
                 }
 
+                // Find the index of the target mapping in the maps list.
+                int targetIndex = maps.FindIndex(m => m.FromId == lockedMap.ToId);
+
+                // If the targetIndex is -1, then the new ID that the old ID is being
+                // mapped to is not being used as an old ID in any of the mappings,
+                // so there is no need to move any of the other IDs around.
+                if (targetIndex < 0)
                 {
-                    // Find the index of the target mapping in the maps list.
-                    int targetIndex = maps.FindIndex(m => m.FromId == lockedMap.ToId);
-                    int currentMaxToId = lockedMap.ToId;
+                    continue;
+                }
 
-                    for (int i = targetIndex; i < mapSize; i++)
+                int currentMaxToId = lockedMap.ToId;
+
+                for (int i = targetIndex; i < mapSize; i++)
+                {
+                    IdMap targetMap = maps[i];
+                    if (targetMap.Locked)
                     {
-                        IdMap targetMap = maps[i];
-                        if (targetMap.Locked)
-                        {
-                            continue;
-                        }
-
-                        while (targetMap.ToId <= currentMaxToId
-                            || lockedTargetToIds.Contains(targetMap.ToId))
-                        {
-                            targetMap.ToId++;
-                        }
-
-                        currentMaxToId = targetMap.ToId;
+                        continue;
                     }
+
+                    while (targetMap.ToId <= currentMaxToId
+                        || lockedTargetToIds.Contains(targetMap.ToId))
+                    {
+                        targetMap.ToId++;
+                    }
+
+                    currentMaxToId = targetMap.ToId;
                 }
             }
         }
 
+        private static void CheckIds(List<(int FromId, int ToId)> idUpdates)
+        {
+            // Check that all the old IDs are unique and all the
+            // new IDs are unique.
+            int fromIds = idUpdates.Select(x => x.FromId).Count();
+            int toIds = idUpdates.Select(x => x.ToId).Count();
 
+            Debug.Assert(fromIds == idUpdates.Select(x => x.FromId).Distinct().Count());
+            Debug.Assert(toIds == idUpdates.Select(x => x.ToId).Distinct().Count());
+            Debug.Assert(fromIds == toIds);
 
+            int negativeFromIds = idUpdates.Select(x => x.FromId).Count(x => x <= 0);
+            int negativeToIds = idUpdates.Select(x => x.ToId).Count(x => x <= 0);
 
+            Debug.Assert(negativeFromIds == 0);
+            Debug.Assert(negativeToIds == 0);
+        }
 
+        private static void CheckIds(List<int> ids)
+        {
+            Debug.Assert(ids.Count == ids.Distinct().Count());
+
+            int negativeIds = ids.Count(x => x <= 0);
+
+            Debug.Assert(negativeIds == 0);
+        }
 
         public static ProjectScenarioModel UpdateActivityIds(
             ProjectScenarioModel projectScenarioModel,
@@ -154,9 +167,6 @@ namespace Zametek.ViewModel.ProjectPlan
             return UpdateActivityIds(projectScenarioModel, [(fromId, toId)]);
         }
 
-
-
-
         public static ProjectScenarioModel UpdateActivityIds(
             ProjectScenarioModel projectScenarioModel,
             List<(int FromId, int ToId)> idUpdates)
@@ -165,49 +175,11 @@ namespace Zametek.ViewModel.ProjectPlan
             ArgumentNullException.ThrowIfNull(idUpdates);
             CheckIds(idUpdates);
 
+            List<int> originalIds = [.. projectScenarioModel.DependentActivities.Select(x => x.Activity.Id).Distinct()];
 
-
-
-
-            // For each ID update, move the existing IDs around so that enough
-            // space exists for the new IDs. But with the minimum changes necessary
-            // to achieve the final IDs specified in the ID updates.
-            //
-            // Make sure to preserve the relative ordering of the old IDs and ensure
-            // that the updates from old to new IDs remains as requested (i.e. they 
-            // are not changed when the existing IDs are moved around to make space
-            // for the new IDs).
-            //
-            // Any of the original IDs that are being updated
-            // should be locked in place so that they are not moved around when
-            // making space for the new IDs.
-            //
-            // For example, ten old IDs numbered 1 to 10. Old ID 3 is mapped to new
-            // ID 4; old ID 4 is mapped to new ID 9; and old ID 5 is mapped to new
-            // ID 8. Whatever happens, those new mappings must be preserved.
-            //
-            // First map old ID 4 to new ID 9, which requires moving old ID 9 to
-            // new ID 10 and old ID 10 to new ID 11. Then map old ID 5 to new ID 8.
-            // But since new ID 9 needs to be preserved, this requires mapping old ID
-            // 8 to new ID 10. However, with this change, old ID 9 should now be mapped
-            // to new ID 11 instead of new ID 10, and old ID 10 should now be mapped
-            // to new ID 12 instead of new ID 11. Finally, map old ID 3 to new ID 4,
-            // which requires no changes to the other mappings since the old ID 4 is
-            // be updated to new ID 9.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            // Update the ID mappings to make sure they are consistent
+            // and make logical sense.
+            idUpdates = UpdateIds(originalIds, idUpdates);
 
             Dictionary<int, int> idUpdatesLookup = idUpdates.ToDictionary(x => x.FromId, x => x.ToId);
 
@@ -295,33 +267,6 @@ namespace Zametek.ViewModel.ProjectPlan
             };
 
             return projectScenarioModel;
-        }
-
-        private static void CheckIds(List<(int FromId, int ToId)> idUpdates)
-        {
-            // Check that all the old IDs are unique and all the
-            // new IDs are unique.
-            int fromIds = idUpdates.Select(x => x.FromId).Count();
-            int toIds = idUpdates.Select(x => x.ToId).Count();
-
-            Debug.Assert(fromIds == idUpdates.Select(x => x.FromId).Distinct().Count());
-            Debug.Assert(toIds == idUpdates.Select(x => x.ToId).Distinct().Count());
-            Debug.Assert(fromIds == toIds);
-
-            int negativeFromIds = idUpdates.Select(x => x.FromId).Count(x => x <= 0);
-            int negativeToIds = idUpdates.Select(x => x.ToId).Count(x => x <= 0);
-
-            Debug.Assert(negativeFromIds == 0);
-            Debug.Assert(negativeToIds == 0);
-        }
-
-        private static void CheckIds(List<int> ids)
-        {
-            Debug.Assert(ids.Count == ids.Distinct().Count());
-
-            int negativeIds = ids.Count(x => x <= 0);
-
-            Debug.Assert(negativeIds == 0);
         }
     }
 }
