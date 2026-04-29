679dade50576412e9233edd86b7911b7c41aa2f2
countincognito <countincognito@outlook.com>
2026-04-29 20:18:52 +0100

    Add a behavior to force the caret to the far end of a textbox in a datagrid cell.

---
+++ src/Zametek.View.ProjectPlan/Miscellaneous/MoveCaretToEndBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+
+namespace Zametek.View.ProjectPlan
+{
+    public class MoveCaretToEndBehavior
+        : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            if (AssociatedObject is not null)
+            {
+                AssociatedObject.GotFocus += OnGotFocus;
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            if (AssociatedObject is not null)
+            {
+                AssociatedObject.GotFocus -= OnGotFocus;
+            }
+        }
+
+        private void OnGotFocus(object? sender, GotFocusEventArgs e)
+        {
+            if (AssociatedObject is { Text: not null } tb)
+            {
+                // Set the cursor to the end of the current text
+                tb.CaretIndex = tb.Text.Length;
+            }
+        }
+    }
+}
