9de7a3691036ef123b8817555541f18122af37c9
countincognito <countincognito@outlook.com>
2026-06-14 15:32:04 +0100

    Separate reset layout display text for docking and graph layouts.

--- src/Zametek.Resource.ProjectPlan/Menus.Designer.cs
+++ src/Zametek.Resource.ProjectPlan/Menus.Designer.cs
@@ -261,9 +261,18 @@ namespace Zametek.Resource.ProjectPlan {
         /// <summary>
         ///   Looks up a localized string similar to Reset Layout.
         /// </summary>
-        public static string Menu_ResetLayout {
+        public static string Menu_ResetDockLayout {
             get {
-                return ResourceManager.GetString("Menu_ResetLayout", resourceCulture);
+                return ResourceManager.GetString("Menu_ResetDockLayout", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Reset Layout.
+        /// </summary>
+        public static string Menu_ResetGraphLayout {
+            get {
+                return ResourceManager.GetString("Menu_ResetGraphLayout", resourceCulture);
             }
         }
         
